53ba7e35c33ad933f91a99162313326a8f475a18
mayura.nawarathne <mayura.nawarathne@payconiq.com>
2024-09-01 17:04:28 +0200

    Initial commit

---
+++ HRAdmistrationAPI/IEmployee.cs
@@ -0,0 +1,9 @@
+namespace HRAdmistrationAPI;
+
+public interface IEmployee
+{
+    int Id { get; set; }
+    string FirstName { get; set; }
+    string LastName { get; set; }
+    decimal Salary { get; set; } 
+}
---
+++ SchoolHRAdmistraion/Program.cs
@@ -0,0 +1,102 @@
+﻿using HRAdmistrationAPI;
+
+namespace SchoolHRAdmistration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal totalSalaries = 0;
+            IList<IEmployee> employees = new List<IEmployee>();
+            SeedData(employees);
+            foreach (IEmployee employee in employees)
+            {
+                totalSalaries += employee.Salary;
+            }
+            Console.WriteLine($"Total Annual Salaries (including bonus): {totalSalaries}");
+            Console.WriteLine($"Total Annual Salaries (including bonus): {employees.Sum(x => x.Salary)}");
+
+        }
+
+        public static void SeedData(IList<IEmployee> employees)
+        {
+            IEmployee teacher1 = new Teacher
+            {
+                Id = 1,
+                FirstName = "Bob",
+                LastName = "Fisher",
+                Salary = 40000
+            };
+            employees.Add(teacher1);
+
+            IEmployee teacher2 = new Teacher
+            {
+                Id = 2,
+                FirstName = "John",
+                LastName = "Smith",
+                Salary = 40000
+            };
+            employees.Add(teacher2);
+
+            IEmployee headOfDepartment = new HeadOfDepartment
+            {
+                Id = 3,
+                FirstName = "Jane",
+                LastName = "Doe",
+                Salary = 50000
+            };
+            employees.Add(headOfDepartment);
+
+            IEmployee deputyHeadMaster = new DeputyHeadMaster
+            {
+                Id = 4,
+                FirstName = "Damien",
+                LastName = "Johnson",
+                Salary = 60000
+            };
+            employees.Add(deputyHeadMaster);
+
+            IEmployee headMaster = new HeadMaster
+            {
+                Id = 5,
+                FirstName = "Devlin",
+                LastName = "Brown",
+                Salary = 70000
+            };
+            employees.Add(headMaster);
+
+        }
+        
+    }
+    public class Teacher : EmployeeBase
+    {
+        public override decimal Salary
+        {
+            get => base.Salary + (base.Salary * 0.02M);
+        }
+    }
+
+    public class HeadOfDepartment : EmployeeBase
+    {
+        public override decimal Salary
+        {
+            get => base.Salary + (base.Salary * 0.03M);
+        }
+    }
+
+    public class DeputyHeadMaster : EmployeeBase
+    {
+        public override decimal Salary
+        {
+            get => base.Salary + (base.Salary * 0.04M);
+        }
+    }
+
+    public class HeadMaster : EmployeeBase
+    {
+        public override decimal Salary
+        {
+            get => base.Salary + (base.Salary * 0.05M);
+        }
+    }
+}
